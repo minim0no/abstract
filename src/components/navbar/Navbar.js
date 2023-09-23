c9521984c2dad7830e7f7ba43dbdbc0336dd0513
Ahmed Idrees <ahmedqidrees@Ahmedqs-MacBook-Air.local>
2023-09-23 17:39:39 -0500

    Implement hamburger menu and sticky navbar

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,50 @@
+import { Link, NavLink } from "react-router-dom";
+import { useState, useEffect } from 'react';
+import { FaBars, FaTimes } from 'react-icons/fa';
+import './Navbar.css';
+
+export default function Navbar() {
+    //checks for scrolling and changes navbar border
+    const [isScrolled, setIsScrolled] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+        if (window.scrollY > 0) {
+            setIsScrolled(true);
+        } else {
+            setIsScrolled(false);
+        }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+        window.removeEventListener('scroll', handleScroll);
+    };
+    }, []);
+
+    //checks for menu open and changes menu icon
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    return (
+        <nav className={`nav ${isScrolled ? 'scrolled' : ''}`}>
+            <div className="left">
+                <Link to="/" className="site-title">abstract</Link>
+                <div className="menu" onClick={() => {
+                    setMenuOpen(!menuOpen);
+                }}>
+                    <FaBars className={"menu-icon"} display={`${!menuOpen ? 'block' : 'none'}`} />
+                    <FaTimes className={"menu-icon"} display={`${menuOpen ? 'block' : 'none'}`} />
+                </div>
+            </div>
+            <div className="right">
+                <ul className={menuOpen ? "open" : ""}>
+                    <li><NavLink to="/">Home</NavLink></li>
+                    <li><NavLink to="/about">About</NavLink></li>
+                    <li><NavLink to="/services">Services</NavLink></li>
+                    <li><NavLink to="/login" id="log-in-button">Login</NavLink></li>
+                </ul>
+            </div>
+        </nav>
+        )
+}
